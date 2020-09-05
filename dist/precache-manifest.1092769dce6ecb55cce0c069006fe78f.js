d6519bd6b951e09b54707b63f12c134cd26b30a7
saad <saad.qadeer@convo.com>
2020-09-05 11:53:13 +0500

    init: init commit

---
+++ dist/precache-manifest.1092769dce6ecb55cce0c069006fe78f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5243a59a4170c6db1c20652887c9e9d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0accc5904e9c0f9ce645",
+    "url": "/static/css/main.bfd9321d.chunk.css"
+  },
+  {
+    "revision": "ff876c4644ad5028196f",
+    "url": "/static/js/2.bf3a6eb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bf3a6eb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0accc5904e9c0f9ce645",
+    "url": "/static/js/main.2c925106.chunk.js"
+  },
+  {
+    "revision": "1dfdbaa47ba20ff4aead",
+    "url": "/static/js/runtime-main.2fb98bc0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
